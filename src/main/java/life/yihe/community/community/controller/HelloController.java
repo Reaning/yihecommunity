1cfc8220e2a925d11b2e36e62b473f4f80f133d1
Lu <lkxBruce@gmail.com>
2021-08-20 14:44:12 +0800

    add index and and bootstrap

--- src/main/java/life/yihe/community/community/controller/HelloController.java
+++ src/main/java/life/yihe/community/community/controller/HelloController.java
@@ -9,10 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/")
 //    @ResponseBody
-    public String hello(@RequestParam(name = "name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    public String hello(){
+        return "index";
     }
 }
