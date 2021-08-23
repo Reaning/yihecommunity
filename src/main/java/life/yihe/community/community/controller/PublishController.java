3d4c903bbed894fc587a4daa69369282ffbac88d
Lu <lkxBruce@gmail.com>
2021-08-23 14:14:16 +0800

    add publish page

---
+++ src/main/java/life/yihe/community/community/controller/PublishController.java
@@ -0,0 +1,14 @@
+package life.yihe.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PublishController {
+    @GetMapping("/publish")
+    public String publish(){
+
+        return "publish";
+    }
+
+}
